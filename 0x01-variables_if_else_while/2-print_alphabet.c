93b0dc74a05b27c3e088de400290c363d93abc3c
Nour Allah <nourallahahmed2002@gmail.com>
2023-06-18 04:37:50 -0700

    Adding the first 2 alphapitical taska

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	char a;
+	
+	for(a = 'a'; a <= 'z'; a ++)
+	{
+		putchar(a);
+		putchar('\n');
+	}
+	return (0);
+}
